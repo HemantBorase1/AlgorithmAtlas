3adb339cc2baf55ff2ab3ff40ef46a8f1f749333
Hemant Borase <hborase004@gmail.com>
2026-08-23 12:49:25 +0530

    Problem5 String

---
+++ Practise/src/main/java/com/String/Problem5.java
@@ -0,0 +1,12 @@
+package com.String;
+
+public class Problem5 {
+    public static void main(String[] args){
+        String str="Hello";
+        int count=0;
+        for(int i=0;i<str.length();i++){
+            count++;
+        }
+        System.out.println(count);
+    }
+}
