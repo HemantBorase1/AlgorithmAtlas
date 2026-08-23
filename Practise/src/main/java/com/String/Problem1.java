ead1d77fe4954729dd24983d84842b6a33e3fdac
Hemant Borase <hborase004@gmail.com>
2026-08-23 12:33:49 +0530

    Problem 1 String

---
+++ Practise/src/main/java/com/String/Problem1.java
@@ -0,0 +1,11 @@
+package com.String;
+
+public class Problem1 {
+
+    public static void main(String[] args){
+        String str="HelloWorld";
+        for(int i=str.length()-1;i>=0;i--){
+            System.out.print(str.charAt(i));
+        }
+    }
+}
