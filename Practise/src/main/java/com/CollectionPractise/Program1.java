12527afeaebd4b540992c6ba5bae1ff9cb177f0a
Hemant Borase <hborase004@gmail.com>
2026-08-26 12:07:36 +0530

    Just Collection Practise

--- Practise/src/main/java/com/CollectionPractise/Program1.java
+++ Practise/src/main/java/com/CollectionPractise/Program1.java
@@ -1,7 +1,9 @@
 package com.CollectionPractise;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.RandomAccess;
 
 public class Program1 {
 
@@ -10,10 +12,14 @@ public class Program1 {
         l.add("Hello");
         l.add(20);
         l.add(1,"Bhasa");
-        List l2=new ArrayList<>();
+        List l2=new ArrayList<>(20);
         l2.add(10);
         l2.add(20);
         l.addAll(l2);
         System.out.println(l);
+        System.out.println(l instanceof RandomAccess); // true
+        System.out.println(l2 instanceof Serializable); // true
+        System.out.println(l instanceof Cloneable);  // true
+        System.out.println(l instanceof RandomAccess); // true
     }
 }
