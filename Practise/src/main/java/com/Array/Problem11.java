0d295c99afc3c153310d01f93185f2526adf628d
Hemant Borase <hborase004@gmail.com>
2026-08-26 14:04:41 +0530

    Problem 11 Array

---
+++ Practise/src/main/java/com/Array/Problem11.java
@@ -0,0 +1,16 @@
+package com.Array;
+
+public class Problem11 {
+    public static void main(String[] args){
+        int arr[]={10,20,30,40,45};
+        int found=45;
+        int index=0;
+        for(int i=0;i<arr.length;i++){
+            if(found==arr[i]){
+                index=i;
+                break;
+            }
+        }
+        System.out.println(found+" Element Found at Index:"+index);
+    }
+}
