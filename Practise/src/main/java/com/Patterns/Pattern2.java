3a7b1928a79f59e7a31029573e62e5a269cf4bec
Hemant Borase <hborase004@gmail.com>
2026-08-30 13:02:28 +0530

    Pattern 2

--- Practise/src/main/java/com/Patterns/Pattern2.java
+++ Practise/src/main/java/com/Patterns/Pattern2.java
@@ -1,4 +1,23 @@
 package com.Patterns;
 
 public class Pattern2 {
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++){
+            for(int j=i;j<5;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
+/*
+Output :
+ *
+ **
+ ***
+ ****
+ *****
+* */
