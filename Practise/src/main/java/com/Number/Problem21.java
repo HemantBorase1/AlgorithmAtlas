e2123bf0f26cb8fc4afdb16f0975e340c2b3e314
Hemant Borase <hborase004@gmail.com>
2026-09-08 09:27:21 +0530

    Date 08-09-2026

---
+++ Practise/src/main/java/com/Number/Problem21.java
@@ -0,0 +1,13 @@
+package com.Number;
+
+public class Problem21 {
+    public static void main(String[] args){
+        int n=1234;
+        int sum=0;
+        while(n!=0){
+            sum+=(n%10);
+            n/=10;
+        }
+        System.out.println("Sum of Digit:"+sum);
+    }
+}
